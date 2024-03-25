e18052d0049a19d0050cbcc8da82f3af31605d4a
Joao Oliveira <joaosantos@aluno.senai.br>
2024-03-25 14:29:53 -0300

    crud initial

--- src/screens/MyPlanets/index.jsx
+++ src/screens/MyPlanets/index.jsx
@@ -4,18 +4,46 @@ import TouchButton from "../../components/TouchButton";
 import styles from "./styles";
 import Title from "../../components/Title";
 
+import PlanetRepository from "../../models/planet/PlanetRepository";
+
 export default function MyPlanets({ route }) {
   const { data } = route.params;
+
+  const editPlanet = () => {
+    navigation.navigate("Form", { planet: data, edit: true });
+  };
+
+  const deletePlanet = () => {
+    PlanetRepository.remove(data.id);
+    navigation.navigate("Planets");
+  };
 
   return (
     <View style={styles.container}>
       <Title title="My Planets" />
 
+      {data ? (
+        <Text>Detalhes do Planeta</Text>
+      ) : (
+        <Text>Selecione um planeta para exibir seus detalhes</Text>
+      )}
+
       <View style={styles.planet}>
-        <Title title="Planet" />
-        <Text style={styles.text}>{data.name}</Text>
-        <Text style={styles.text}>{data.governante}</Text>
-        <Text style={styles.text}>{data.loc}</Text>
+        <View style={styles.planetDetail}>
+          <Title title="Planet" />
+          <Text style={styles.text}>{data.name}</Text>
+          <Text style={styles.text}>{data.governante}</Text>
+          <Text style={styles.text}>{data.loc}</Text>
+        </View>
+
+        <View style={styles.planetActions}>
+          <TouchableOpacity style={styles.editButton} onPress={editPlanet}>
+            <Text>Editar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.deleteButton} onPress={deletePlanet}>
+            <Text>Excluir</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
